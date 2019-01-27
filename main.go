d83bb3e3222b2103dadf777bdde4418fef0fcd1e
Thomas Alfa Edison <thomas.alfa.edison@gmail.com>
2019-01-27 20:33:56 +0700

    Firts Commit

---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
+)
+
+func main()  {
+
+	mysqlBinDir := "C:/xampp71/mysql/bin"
+	database := "tunjangan-babelprov"
+	outputDir := "C:/goprojects"
+
+	waktu := time.Now()
+
+	baseName := fmt.Sprintf("%s-%d-%02d-%02d-%02d",database,waktu.Year(),
+		waktu.Month(),waktu.Day(),waktu.Hour())
+
+	sqlOutputFile := fmt.Sprintf("%s/%s.sql",outputDir,baseName)
+
+	zipOutputFile := fmt.Sprintf("%s/%s.zip",outputDir,baseName)
+
+	mysqldump(mysqlBinDir,database,sqlOutputFile)
+
+	zipFile(sqlOutputFile,zipOutputFile)
+
+}
+
+func zipFile(sourceFile string, targetFile string) {
+
+	newZipFile, err := os.Create(targetFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer newZipFile.Close()
+
+	zipWriter := zip.NewWriter(newZipFile)
+	defer zipWriter.Close()
+
+	zipfile, err := os.Open(sourceFile)
+	defer zipfile.Close()
+
+	info, err := zipfile.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	header, err := zip.FileInfoHeader(info)
+
+	header.Name = filepath.Base(sourceFile)
+	header.Method = zip.Deflate
+
+	writer, err := zipWriter.CreateHeader(header)
+
+	_, err = io.Copy(writer, zipfile)
+
+}
+
+func mysqldump(mysqlBinDir string, database string, sqlOutputFile string) string {
+	args := []string{"-u","root",database}
+	cmd := exec.Command("mysqldump",args ...)
+	cmd.Dir = mysqlBinDir
+
+	stdout, err := cmd.StdoutPipe()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	bytes, err := ioutil.ReadAll(stdout)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(sqlOutputFile, bytes, 0644)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return sqlOutputFile
+
+}
